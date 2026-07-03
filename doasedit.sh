be873a8c739935c107be0c6be2b6acf618b5d995
Mark Oborne <markoborne1@gmail.com>
2026-07-04 08:33:57 +0900

    fix: fix doasedit

--- doasedit.sh
+++ doasedit.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
-set -euo pipefail
+set -eu
 
 file=$1
 
 temp=$(mktemp doasedit.XXXXX)
 cp "$file" "$temp"
+# Ensure we get a chance to enter password first. Assumes persist is enabled.
+doas true
 $EDITOR "$temp"
-doas mv "$temp" "$file"
+# cp over mv to maintain metadata
+doas cp "$temp" "$file"
+rm "$temp"
